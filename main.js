c9d436ad71a7a43e9b7a04a6030e5c0dc8be28bd
victormtz-dev <l17320909@acapulco.tecnm.mx>
2021-10-05 23:40:27 -0500

    Usando async await

--- main.js
+++ main.js
@@ -15,59 +15,60 @@ window.addEventListener("load", () => {
 
       const apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang=es`;
 
-      fetch(apiURL)
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-          let temp = Math.round(data.main.temp);
-          temperaturaValor.textContent = `${temp} °C`;
+      const apiFetch  = async ()=> {
+          try {
 
-          let desc = data.weather[0].description;
-          temperaturaDes.textContent = desc.toUpperCase();
+            const urlapi = await fetch(apiURL);
+            const data = await urlapi.json();
 
-          ubicacion.textContent = data.name;
-
-          velocidad.textContent = `${data.wind.speed} m/s`;
-        const arregloIconos = ["Thunderstorm", "Drizzle", "Rain", "Snow", "Atmosphere", "Clear", "Clouds"];
-
-
-
-          switch (data.weather[0].main) {
-            case "Thunderstorm":
-              iconoAnimado.src = "animated/thunder.svg";
-              break;
-            case "Drizzle":
-              iconoAnimado.src = "animated/rainy-2.svg";
-
-              break;
-            case "Rain":
-              iconoAnimado.src = "animated/rainy-7.svg";
-
-              break;
-            case "Snow":
-              iconoAnimado.src = "animated/snowy-6.svg";
-
-              break;
-            case "Clear":
-              iconoAnimado.src = "animated/day.svg";
-
-              break;
-            case "Atmosphere":
-              iconoAnimado.src = "animated/weather.svg";
-
-              break;
-            case "Clouds":
-              iconoAnimado.src = "animated/cloudy-day-1.svg";
-
-              break;
-            default:
-              iconoAnimado.src = "animated/cloudy-day-1.svg";
+            let temp =  Math.round(data.main.temp);
+            temperaturaValor.textContent = `${temp} °C`;
+  
+            let desc = data.weather[0].description;
+            temperaturaDes.textContent = desc.toUpperCase();
+  
+            ubicacion.textContent = data.name;
+  
+            velocidad.textContent = `${data.wind.speed} m/s`;
+          
+  
+            switch (data.weather[0].main) {
+              case "Thunderstorm":
+                iconoAnimado.src = "animated/thunder.svg";
+                break;
+              case "Drizzle":
+                iconoAnimado.src = "animated/rainy-2.svg";
+  
+                break;
+              case "Rain":
+                iconoAnimado.src = "animated/rainy-7.svg";
+  
+                break;
+              case "Snow":
+                iconoAnimado.src = "animated/snowy-6.svg";
+  
+                break;
+              case "Clear":
+                iconoAnimado.src = "animated/day.svg";
+  
+                break;
+              case "Atmosphere":
+                iconoAnimado.src = "animated/weather.svg";
+  
+                break;
+              case "Clouds":
+                iconoAnimado.src = "animated/cloudy-day-1.svg";
+  
+                break;
+              default:
+                iconoAnimado.src = "animated/cloudy-day-1.svg";
+            }        
+          } catch (error) {
+              console.log(error);
           }
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+
+      }
+      apiFetch();
     });
   }
 });
